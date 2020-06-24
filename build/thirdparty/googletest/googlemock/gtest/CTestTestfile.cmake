b35a88b349973cd90ef83aacab35dc794a1bc010
Mahmoud Abdelbary <mahmoud.abdelbaty@gmail.com>
2020-06-24 18:29:54 +0200

    adding rest of the project

---
+++ build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/workspace/git/cpp_learning_projects/udacity C++ nanoDegree/rotuePlannerProject/thirdparty/googletest/googletest
+# Build directory: /home/workspace/git/cpp_learning_projects/udacity C++ nanoDegree/rotuePlannerProject/build/thirdparty/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
